f07c243830559d3bdf4fc7c5fea7f83238f2dc44
ZixinMao123 <112448588+ZixinMao123@users.noreply.github.com>
2024-03-28 22:17:34 -0500

    Add files via upload

---
+++ dynamic_dubins_car.py
@@ -0,0 +1,146 @@
+import numpy as np
+from scipy.integrate import odeint
+from math import sin, cos, tan, atan, pi, sqrt, ceil
+
+
+
+# Define these globally if they are to be accessed in functions below
+
+
+def initialize_reference_trajectory_and_input():
+	global k,T,dt,length,G,kappa,velocity
+	G = 100
+	kappa = 10
+	length = 1
+	dt = 1 # Timestep
+	#T = 10 # Total time for this part of the motion
+	velocity = 2 # Constant velocity
+	k = 100 # Damping coefficient from the dynamics
+	
+	# Initial conditions and computation
+	
+
+# Make sure to call this function before running simulations or functions that depend on ref_traj and ref_input
+initialize_reference_trajectory_and_input()
+
+def ref_traj(t):
+	output=[] 
+	x_initial=0
+	y_initial =0
+	heading_initial=0
+	ref_turning =0
+	x_ref =x_initial + t*velocity
+	y_ref=y_initial +t*velocity
+	heading_ref = heading_initial
+	return [x_ref, y_ref, heading_ref, velocity, ref_turning]
+
+	
+def ref_input(t):
+	res =[]
+	k = 100  # Damping coefficient from the dynamics
+	con_velocity = velocity
+	force = k * con_velocity
+	return [force,con_velocity]
+	
+# Now ref_traj and ref_input are ready to be used in your simulations or functions
+
+def dynamics(state, t, input):
+        x,y,heading,vel,turning = state
+        force,torque = input
+
+        xdot = vel*cos(heading)
+        ydot = vel*sin(heading)
+        headingdot = turning
+        veldot = force - k*vel
+        turningdot = torque - k*turning
+
+        return [xdot, ydot, headingdot, veldot, turningdot]
+
+def trackingController(state, ref_state, ref_input):
+        x,y,heading,vel,turning = state
+        xref,yref,headingref,velref,turningref = ref_state
+
+        forceref,torqueref = ref_input
+
+        aref = (forceref - k*velref)
+
+        zx = x + length*cos(heading) - (xref + length*cos(headingref))
+        zy = y + length*sin(heading) - (yref + length*sin(headingref))
+
+        vx = velref*cos(heading) - length*sin(headingref)*turningref
+        vy = velref*sin(heading) + length*cos(headingref)*turningref
+
+        ax = aref*cos(headingref)
+        ay = aref*sin(headingref)
+
+        gx = zx*(G/(1 + np.linalg.norm(np.array([zx, zy]))))
+        gy = zy*(G/(1 + np.linalg.norm(np.array([zx, zy]))))
+
+        ux = ax + k*vx - gx
+        uy = ay + k*vy - gy
+
+        force = cos(heading)*(ux + vel*turning*sin(heading)) + length*(turning**2)*cos(heading) + sin(heading)*(uy - vel*turning*cos(heading) + length*(turning**2)*sin(heading)) + length*turning**2
+        torque = (-sin(heading)/length)*(ux + vel*turning*sin(heading) + length*(turning**2)*cos(heading)) + (cos(heading)/length)*(uy - vel*turning*cos(heading) + length*(turning**2)*sin(heading)) - (vel*turning/length)
+
+        return [force, torque]
+
+def controlledDynamics(state, t):
+        ref_idx = ceil(t/dt)
+        if ref_idx >= len(ref_traj):
+           ref_idx = len(ref_traj)-1
+
+        ref_state = ref_traj[ref_idx]
+        ref_input = ref_input[ref_idx]
+
+        input = trackingController(state, ref_state, ref_input)
+        xdot, ydot, headingdot, veldot, turningdot = dynamics(state, t, input)
+
+        return [xdot, ydot, headingdot, veldot, turningdot]
+
+def errorDynamics(state, t):
+        ref_idx = ceil(t/dt)
+        res =[]
+        ref_state1 = ref_traj(ref_idx)
+        ref_input1 = ref_input(ref_idx)
+        input = trackingController(state, ref_state1, ref_input1)
+        xdot, ydot, headingdot, veldot, turningdot = dynamics(state, t, input)
+        xdot1, ydot1, headingdot1, veldot1, turningdot1 = dynamics(ref_state1, t, ref_input1)
+        res =[xdot-xdot1,ydot-ydot1,headingdot-headingdot1,veldot-veldot1,turningdot-turningdot1]
+        return res
+
+def run_simulation( initial_state, T, dt):
+	number_p = int(np.ceil(T/dt))
+	T = float(T)
+	t = [i*dt for i in range(0,number_p)]
+	if t[-1] != dt:
+		t.append(T)
+	newt = []
+	for step in t:
+		newt.append(format(step, '.2f')
+	#t = newt
+	x_init = initial_state[0]
+	y_init = initial_state[1]
+	head_init = initial_state[2]
+	vel_init = initial_state[3]
+	turn_init = initial_state[4]
+	initial_ode = [x_init,y_init,head_init,vel_init,turn_init]
+	sol = odeint(errorDynamics, initial_ode, newt, hmax=dt)
+
+	trace = []
+	for i in range(len(newt)):
+		tmp =[]
+		tmp.append(newt[i])
+		tmp.append(sol[i,0])
+		tmp.append(sol[i,1])
+		tmp.append(sol[i,2])
+		tmp.append(sol[i,3])
+		tmp.append(sol[i,4])
+		trace.append(tmp)
+	return trace
+
+
+    # TODO: Implement error bounds
+    # TODO: Implement error dynamics
+    # TODO: Implement TC Simulate for error dynamics (Can be used with DryVr)
+    # TODO: Implement simulations (particularly dynamic-FACTEST simulation)
+    
---
+++ system_dubin.py
@@ -0,0 +1,70 @@
+import sys
+sys.path.append('..')
+ 
+from dynamic_dubins_car import run_simulation
+import numpy as np
+from scipy.integrate import odeint
+import scipy as scipy
+import scipy.special as sc
+import random
+
+TMAX = 10
+dt = 1
+
+# range of initial states
+
+lower = np.array([-10, -10, 0, 0, 0])
+higher = np.array([10, 10, 0, 5, 0])
+
+
+X0_center_range = np.array([lower, higher]).T
+X0_r_max = 0.5
+
+    
+def sample_X0():
+    center = X0_center_range[:,0] + np.random.rand(X0_center_range.shape[0]) * (X0_center_range[:,1]-X0_center_range[:,0])
+    r = np.random.rand()*X0_r_max
+    X0 = np.concatenate([center, np.array(r).reshape(-1)])
+    return X0
+
+def sample_t():
+    return (np.random.randint(int(TMAX/dt))+1) * dt
+
+def sample_x0(X0):
+    center = X0[:-1]
+    r = X0[-1]
+
+    n = len(center)
+    direction = np.random.randn(n)
+    direction = direction / np.linalg.norm(direction)
+
+    x0 = center + direction * r
+    x0[x0>X0_center_range[:,1]] = X0_center_range[x0>X0_center_range[:,1],1]
+    x0[x0<X0_center_range[:,0]] = X0_center_range[x0<X0_center_range[:,0],0]
+    return x0
+
+def sample_x0_uniform(X0):
+    center = X0[:-1]
+    r = X0[-1]
+
+    n = len(center)
+    direction = np.random.randn(n)
+    direction = direction / np.linalg.norm(direction)
+
+    dist = np.random.rand()
+    x0 = center + direction * dist * r
+    x0[x0>X0_center_range[:,1]] = X0_center_range[x0>X0_center_range[:,1],1]
+    x0[x0<X0_center_range[:,0]] = X0_center_range[x0<X0_center_range[:,0],0]
+    return x0
+
+def simulate(x0):
+    return np.array(run_simulation(x0, sample_t(),dt))
+
+def get_init_center(X0):
+    center = X0[:-1]
+    return center
+
+def get_X0_normalization_factor():
+    mean = np.zeros(len(sample_X0()))
+    std = np.ones(len(sample_X0()))
+    return [mean, std]
